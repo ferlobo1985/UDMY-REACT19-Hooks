eefbe15f5b80e4b8f9a4faea302e1a0fa5bffe7f
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-06 02:03:10 -0300

    02-UseEffect
    02-UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
   let [count,setCount] = useState(0);
+  let [animal,setAnimal] = useState('dog');
 
   const addOne = () => setCount(count+1);
   const subtOne = () => setCount(count-1);
   const reset = () => setCount(0);
+
+  const doSomething = () => {
+    console.log('do something')
+  }
+
+  useEffect(()=>{
+    console.log('run once')
+  },[])
+
+
+  useEffect(()=>{
+    console.log(count)
+  },[count])
+
+  useEffect(()=>{
+    console.log(animal)
+  },[animal])
 
   return(
     <>
@@ -13,6 +31,11 @@ const App = () => {
       <button onClick={addOne}>+1</button>
       <button onClick={subtOne}>-1</button>
       <button onClick={reset}>Set to cero</button>
+      <hr/>
+      <h3>{animal}</h3>
+      <button onClick={()=>setAnimal('cat')}>
+        Change animal
+      </button>
     </>
   )
 }
